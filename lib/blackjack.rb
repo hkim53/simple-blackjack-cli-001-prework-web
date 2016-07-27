2b17cab6439a3fc8ebfec7e94315438237d29c53
hkim53 <paragon.hankido@gmail.com>
2016-07-27 14:57:58 -0400

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -3,7 +3,8 @@ def welcome
 end
 
 def deal_card
-  rand(1..11)
+  card = rand(1..11)
+  return card
 end
 
 def prompt_user
@@ -11,13 +12,11 @@ def prompt_user
 end
 
 def display_card_total(card_total)
- puts "Your cards add up to #{card_total}."
+  puts "Your cards add up to #{card_total}"
 end
 
-
 def get_user_input
-  user_input = gets.chomp
-  puts "#{user_input}"
+  gets.chomp
 end
 
 def end_game(card_total)
@@ -25,30 +24,30 @@ def end_game(card_total)
 end
 
 def initial_round
-  display_card_total = deal_card + deal_card
-  puts display_card_total
-  return display_card_total
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
+  return card_total
 end
 
 def hit?(card_total)
   prompt_user
   input = get_user_input
-  if input == "s"
-    card_total
-  elsif input == "h"
-    card_total += deal_card
-  else
-    invalid_command
-  end
+    if input == "s"
+      card_total
+    elsif input == "h"
+      card_total += deal_card
+    else
+      invalid_command
+    end
   return card_total
 end
 
 def invalid_command
-  puts "Please enter a valid command"
+  puts "Please enter a valid command."
 end
 
-def play_again?
-  puts "Another attempt? y or n"
+def play_again
+  puts "Would you like to play again? y or no"
   yes_or_no
 end
 
@@ -57,30 +56,27 @@ def yes_or_no
   if input == "y"
     runner
   elsif input == "n"
-    puts "Yay"
-  else invalid_command
+    puts "Thanks for playing!"
+  else
+  invalid_command
   end
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+
 
 def runner
   welcome
   card_total = initial_round
-  loop
+  loop do
     if card_total < 21
-      card_total = hit?(card_total)
-      display_card_total(card_total)
+       card_total = hit?(card_total)
+       display_card_total(card_total)
     elsif card_total == 21
-      puts "You win!"
-      return play_again
+       puts "Blackjack!"
+       return play_again
     elsif card_total > 21
-      end_game(card_total)
-      return play_again
+       end_game(card_total)
+       return play_again
     end
   end
-  
-
-    
+end
